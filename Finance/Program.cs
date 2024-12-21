0105f76540186dcb2492275ae8964e9e7c7d7ebd
binsu93 <binsumpeter@gmail.com>
2024-12-21 11:18:59 +0530

    Initial Commit

---
+++ Finance/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Finance;
+
+Console.WriteLine("Hello, World!");
+
+double p = 0;
+double r = 0;
+double n = 0;
+
+Console.WriteLine("Enter Principle Amount");
+p = double.Parse(Console.ReadLine());
+Console.WriteLine(" Annual Enter Interest ");
+r = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter Tenure in months");
+n = double.Parse(Console.ReadLine());
+
+double  mr = r / 12/100;
+
+double mysr = Math.Pow((1 + mr), n) / ((Math.Pow((1 + mr), n)) - 1);
+double seconfrr= Math.Pow((1 + mr), (n - 1));
+
+
+double emi = 0;
+emi = p * mr * mysr;
+double updatedP = p;
+int un = 0;
+do
+{
+    double mnthintsrt = updatedP * mr;
+    double reducingPrincipal = emi - mnthintsrt;
+    updatedP = updatedP - reducingPrincipal;
+    updatedP -= 2000;
+    un++;
+
+
+} while (updatedP > 0);
+parent obj = new child();
+obj.print();
+Console.WriteLine("Your Emi is :  " + emi);
+Console.WriteLine("Your total term is :  " + un);
+
+
+
---
+++ Finance/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance
+{
+   public class parent
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("Parent");
+        }
+    }
+    public class child:parent
+    {
+        public override void print()
+        {
+            Console.WriteLine("child");
+        }
+    }
+}
